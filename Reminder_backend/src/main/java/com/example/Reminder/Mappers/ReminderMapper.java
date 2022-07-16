c35aaed1da142bfd699cc70d56b7cb3857fedc56
BhasuruKrishnaChaitanya <bhasuruchaitanya@gmail.com>
2022-07-16 17:44:20 +0530

    backend changes

--- Reminder_backend/src/main/java/com/example/Reminder/Mappers/ReminderMapper.java
+++ Reminder_backend/src/main/java/com/example/Reminder/Mappers/ReminderMapper.java
@@ -2,7 +2,10 @@ package com.example.Reminder.Mappers;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.Reminder.RemindRequest;
@@ -10,8 +13,20 @@ import com.example.Reminder.RemindRequest;
 @Mapper
 public interface ReminderMapper {
 	
-	@Select("select title as title, message as message , user_id as userId from reminder")
+	@Select("select title as title, message as message , reference_id as referenceId from reminder")
     public List < RemindRequest > findAll();
 	
+	@Insert({ "<script>",
+		"insert into reminder (TITLE, MESSAGE, REFERENCE_ID, NOTIFY_TIME, STATUS) values ",
+		"<foreach collection='reminderDetails' item='reminder' index='index' open='(' separator = '),(' close=')' >"
+		+"#{reminder.title},#{reminder.message},#{account.referenceId},#{account.notifyTime},#{account.status}"
+		+"</foreach>",
+		"</script>" })
+	public void insertBulkCustomerDetails(@Param("reminderDetails") List<RemindRequest> reminderDetails);
 	
+	@Select("select title as title, message as message , reference_id as referenceId from reminder where referenceId = #{referenceId}")
+    public List < RemindRequest > findUser(@Param("referenceId") String userId);
+	
+	@Delete("delete from reminder where reference_id=#{referenceId}")
+	void deleteBooks(@Param("userId") String userId);
 }
---
+++ Reminder_backend/src/main/java/com/example/Reminder/Services/ReminderServices.java
@@ -0,0 +1,5 @@
+package com.example.Reminder.Services;
+
+public class ReminderServices {
+
+}
